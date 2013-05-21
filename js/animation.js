cbfd9a147dcb4be16165458d330dcc42eed258a7
yannikmesserli <yannik.messerli@gmail.com>
2013-05-21 22:50:20 +0200

    Loop

---
+++ js/animation.js
@@ -0,0 +1,283 @@
+'use_strict';
+
+/*
+	Class Loop that will help animate infinitely the different component.
+    The constructor has a period as parameter, which will synchronise every object together.
+
+    The first animation is fire when the start command is started.
+
+    Rewind let play the animation in a back and forth manner
+ */
+var Loop = function(){
+	
+    // period to synch all the Loop together:
+    var period = 1700;
+
+    // Keep track of all the animation we want to play:
+    var animations = [];
+
+    // Is the animation looping?
+    var isRunning = false;
+
+    // Current index of the animations that we are running:
+    var indexAnim = 0;
+
+    // Increment to have the right direction 
+    var inc_signed = 1;
+
+    // Function that add an object animation to the list of the animations to do
+    var add = function(animation, i){
+        if(!isRunning){
+
+            // If no end has been setup:
+            if(animation.end === undefined){
+                animation.end = 1.0;
+            }
+            // If no animation, just pass a blank one:
+            if(animation.Movement === undefined || animation.Movement.start === undefined){
+                animation.Movement = new Movement();
+            }
+
+            animations[i] = animation;
+        }
+    }
+
+	this.add = function(animations){
+        $.each(animations, function(index, anim){
+            add(anim, index);
+        });
+        // for chaining:
+        return this;
+	}
+
+
+    this.start = function(){
+        isRunning = true;
+
+        // Randomize the duration:
+        var durations = [];
+        var accum = 0.0;
+        for (var i = 0; i < animations.length - 1; i++) {
+            durations[i] = Math.random() / (animations.length-1) * period;
+            accum += durations[i];
+        };
+        durations[animations.length - 1] = period - accum;
+
+        // Function recursive to chain the animations
+        function prepareNext(index){
+             
+            var anim = animations[index];
+
+            anim.Movement.once('start', function(curPos){
+                if(anim.init === undefined)
+                    anim.init = curPos;
+                indexAnim = index;
+            });
+
+            anim.Movement.once('terminate', function(){
+                // Swap init and end:
+                var tmp = anim.init;
+                anim.init = anim.end;
+                anim.end = tmp;
+
+                var next_anim = index + inc_signed;
+                
+                if( next_anim >= animations.length ) {
+                    inc_signed = -1;
+                    next_anim = index;
+                }
+                if( next_anim < 0.0 ) {
+                    inc_signed = 1.0;
+                    next_anim = index;
+                }
+
+                
+                
+                // recursively call this function:
+                prepareNext(next_anim);
+            });
+
+            // Start this current animation
+            anim.Movement.start(anim.end, anim.duration * period );
+        }
+
+        prepareNext(indexAnim);
+
+        return this;
+    }
+
+    this.stop = function(){
+        isRunning = false;
+        // Stop any animation
+        $.each(animations, function(index, anim) {
+            anim.Movement.stop();
+        });
+        // we need also to reset
+        // 
+        
+        return this;
+    }
+	
+}
+
+
+var Movement = function(min, max, callback){
+	// List of animations registered:
+	var interface = new Item();
+	interface._draw = function(){};
+
+
+	var max = max || 1.0;
+	var min = min || 0.0;
+	
+    // Setup a Movement that does not nothing:
+	if(callback === undefined ){    
+        var callback = function(){} 
+    };
+
+	interface.currentPos = 0.0;
+	interface.isRunning = false;
+
+	smokesignals.convert(interface);
+
+	// find the time when we fire this function:
+    var startingTime = -1;
+    
+
+    // Animate it:
+    // Intern class
+    var Animation = function(duration, _from, to){
+            
+    	interface.isRunning = true;
+    	// Stupid var to catch the time:
+	var isNotSet = true;
+            // reset
+            startingTime = -1;
+            // Percent done for this Animation:
+            var percDone = 0.0;
+            // Test direction
+            var goPositive = (to - _from > 0.0);
+            // Start from the value _from or from currentPos if already partially done:
+            var from =  goPositive? Math.max(interface.currentPos, _from): Math.min(interface.currentPos, _from);
+           
+    	// Return the function to execute:
+    	return function(evt){
+    		if(percDone < 1.0){
+            		// Catch the time:
+            		if(isNotSet) {
+            			startingTime = evt.time;
+            			isNotSet = false;
+            		}
+
+            		// Where we stand at, in percent?
+                            percDone =  (evt.time - startingTime)/duration * 1000;
+            		
+                            // Handle movement at the end:
+            		if(percDone > 1.0 ) {
+                                    // console.log(to);
+                                    percDone = 1.0;
+                                    interface.stop('terminate');
+            		}
+
+                            // Update the position of the movement:
+                            interface.currentPos = percDone * to  + (1 - percDone) * from;
+    	        	// Call the worker, do the real stuff:
+    	        	callback( min + max * interface.currentPos );
+                            // Tick
+                            interface.emit('tick', percDone, interface.currentPos);
+                    }
+    	        			
+    	}
+    }
+
+
+    // Control:
+    interface.start = function(a, b, c){
+
+        //Function to test the parameters:
+        var testVar = function(a){
+                return ((a > 0.0) && (a < 1.0));
+        }
+
+        // 0 parameter:
+        var duration = 1000;
+        var from = interface.currentPos;
+        var to = 1.0;
+
+        // if only one parameter:
+        if( a != null && b == null && c == null){
+                if(testVar(a)) to = a;
+        }
+
+        // if two parameters
+         if( a != null && b != null && c == null){
+                if(testVar(a)) to = a;
+                if(b > 0.0) duration = b;
+        }
+
+        // if three parameters
+         if( a != null && b != null && c != null){
+                if(testVar(a)) to = a;
+                if(testVar(b)) to = b;
+                if(c > 0.0) duration = c;
+        }
+    	
+
+    	if(interface.isRunning) {
+                    interface.stop('interupted');
+            }
+
+        // Emit event starting:
+        interface.emit('start', interface.currentPos);
+
+    	interface.onFrame = new Animation(duration, from, to);
+
+           
+
+            // For chaining purpose
+            return interface;
+    }
+
+
+    interface.stop = function(stop_reason, to){
+
+    	interface.isRunning = false;
+            interface.emit('stop');
+            interface.emit(stop_reason);
+            //interface.onFrame = null; // Weird bug, if I set up it to null, animation got not reassigned
+    	interface.onFrame = function(){};
+            // For chaining purpose
+            return interface;
+    }
+
+
+
+    interface.set = function(_perc){
+
+    	if(interface.isRunning) interface.stop();
+
+    	var perc = _perc || 0.0;
+    	interface.currentPos = perc;
+
+    	callback( min + max * interface.currentPos );
+
+            // For chaining purpose
+            return interface;
+    }
+
+    interface.resume = function(){
+    	interface.set(0.0);
+
+            // For chaining purpose
+            return interface;
+    }
+	
+
+	// Start:
+	//interface.set(0.0);
+
+	
+
+	return interface;
+
+}
--- js/body.js
+++ js/body.js
@@ -1,55 +1,342 @@
+'use_strict';
+
+
+
+
+/*
+    Be careful, when you assign multiple variables with ','
+    in a class, it, for some reasons, share this variable with all the
+    instance...
+ */
+
 with (paper){
-    var Dancer = function(center){
+    var Dancer = function(base, period ){
 
-        // Just draw a body first:
-        body();
+        /*
+            - base: is the point from which every other point is attached to
+            - perdio: is the time of the beat from the music. Each dancer is supposed to be sync with it (some of them could be bad dancer and not snyc with it)
 
-        // Interface:
-        return {
+         */
 
-        };
-    }
+        var _dancer = new Group();
 
 
-    var arm_front = function(){
 
-        // Interface:
-        return {
 
-            // Execute when enter the frame:
-            onFrame: function(){
 
-            }
+        // Constructor:
+        var init = function(){
+            
+            // Just draw a body first:
+            var o = body();
+           
+            var left_arm = new arm( new Point(120, 250));
+           
+            var right_arm = new arm( new Point(280, 250));
+            _dancer.addChildren([o, left_arm, right_arm]);
+            
+
+            // setup a new loop:
+           new Loop().add(
+                    [
+                        {
+                            'end': 0.7,
+                            'Movement':  left_arm.foldElbow,
+                            'duration': 0.2
+                        },
+                        {
+                            'end': 0.1,
+                            'Movement':  left_arm.foldElbow,
+                            'duration': 0.5
+                        },
+                        {
+                            'end': 0.4,
+                            'Movement':  left_arm.foldElbow,
+                            'duration': 0.3
+                        }
+                    ]
+                ).start();
+
+
+           new Loop().add(
+                    [
+                        {
+                            'end': 0.7,
+                            'Movement':  right_arm.foldElbow,
+                            'duration': 0.2
+                        },
+                        {
+                            'end': 0.1,
+                            'Movement':  right_arm.foldElbow,
+                            'duration': 0.5
+                        },
+                        {
+                            'end': 0.4,
+                            'Movement':  right_arm.foldElbow,
+                            'duration': 0.3
+                        }
+                    ]
+                ).start();
+
+            new Loop().add(
+                    [
+                        {
+                            'end': 0.4,
+                            'Movement':  left_arm.rotate,
+                            'duration': 0.5
+                        },
+                        {
+                            'end': 0.9,
+                            'Movement':  left_arm.rotate,
+                            'duration': 0.5
+                        }
+                    ]
+                ).start();
+
+
+            new Loop().add(
+                    [
+                        {
+                            'end': 0.4,
+                            'Movement':  right_arm.rotate,
+                            'duration': 0.5
+                        },
+                        {
+                            'end': 0.9,
+                            'Movement':  right_arm.rotate,
+                            'duration': 0.5
+                        }
+                    ]
+                ).start();
+
+           
+            
+            // left_arm.rotate.start(0.8, 500).once('terminate', function(){
+            //     left_arm.foldElbow.start(0.7, 200).once('terminate', function(){
+            //         left_arm.foldElbow.start(0.6, 0.1, 400);
+            //     });
+            // });
+
+            
+            right_arm.foldElbow.start(0.5, 400);
+
+
+            return _dancer;
+
         }
-    }
 
-    
-    var arm_back = function(){
+   
 
-        // Interface:
-        return {
 
-            // Execute when enter the frame:
-            onFrame: function(){
 
+
+
+        // The arms:
+        var arm = function(anchor, options){
+
+            /* Arm has 3 main points:
+             * - the starting point
+             * - the elbow point
+             * - the hand point
+             *
+             * Depending of the point, we use
+             * the arm_front and arm_back to draw it
+             */
+
+            var pt = {};
+            var _arm = new Group();
+
+            // Add event handler:
+            smokesignals.convert(_arm);
+
+            var default_settings = {
+                elbow: new Point(0, 100),
+                end: new Point(0, 100),
+                rotate: {
+                    max: 200,
+                    min: 90
+                },
+                foldElbow: {
+                    max: 160,
+                    min: 0
+                }
+            };
+
+            var settings = $.extend({}, default_settings, options || {}) ;
+
+            /* ******************************************* /
+             * Register possible movement:
+             *
+             */
+            
+
+            // rotation of the arm in respect to the body:
+            _arm.rotate = new Movement(settings.rotate.min, settings.rotate.max , function(val){
+                
+                var vect = pt.elbow.subtract(pt.begin);
+                var vect2 = pt.end.subtract(pt.elbow);
+                vect2.angle = vect2.angle - vect.angle + val;
+                vect.angle = val;
+                pt.elbow = vect.add(pt.begin);
+                pt.end =  vect2.add(pt.elbow); 
+                _arm.emit('changed');
+
+            }).on('interupted', function(){
+                //console.log('interupted');
+            });
+
+
+            // Fold the Elbow with respect to the arm:
+            _arm.foldElbow = new Movement(settings.foldElbow.min, settings.foldElbow.max, function(beta){
+                // Get the current orientation of the arm:
+                var vect1 = pt.elbow.subtract(pt.begin);
+                var alpha = vect1.angle;
+                // change the upper part:
+                var vect2 = pt.elbow.subtract(pt.begin);
+                vect2.angle = alpha + beta;
+                pt.end =  vect2.add(pt.elbow); 
+                _arm.emit('changed');
+
+            }).on('terminate', function(){
+                //console.log('terminate');
+            });
+            
+            var init = function(){
+                construct_point();
+       
+                var myfront = new arm_front();
+                var myback = new arm_back();
+                
+                _arm.addChildren([myfront, myback ]);
+
+                return _arm;
+            };
+
+
+            var construct_point = function(){
+                pt.begin = anchor || new Point(10,0);
+                pt.elbow = pt.begin.add(settings.elbow);
+                pt.end = pt.elbow.add(settings.end);
             }
-        }
-    }    
 
-    var arm = function(){
 
-        // Interface:
-        return {
 
-            // Execute when enter the frame:
-            onFrame: function(){
 
+            // A generic function to create arm:
+            var armGen = function(starting, ending, _draw){
+
+                // A reference to point creating the structure of the arm:
+                var __starting_point__ = starting;
+                var __ending_point__ = ending;
+                
+
+
+                // The arm is actually a group:
+                var part_arm = new Group();
+
+                var draw = _draw || function(start, end){
+                   
+                    var tmp = new Path([ start, end] );
+                    tmp.selected = true;
+                    return [ tmp ];
+                };
+
+                var init = function(){
+                    // Draw stuff:
+                    part_arm.updateView();
+                    // Register event:
+                    _arm.on('changed', function(){
+                        part_arm.updateView();
+                    })
+
+                    return part_arm;
+                }
+
+                part_arm.updateView = function(){
+ 
+                    part_arm.removeChildren();
+                    part_arm.addChildren( draw( pt[__starting_point__], pt[__ending_point__]) );
+
+                }
+
+
+                part_arm.moveStartTo = function(new_p, t){
+                    moveGenTo(__starting_point__, __ending_point__, new_p, t);
+                }
+
+                part_arm.moveEndTo = function(new_p, t){
+                    moveGenTo(__ending_point__, __starting_point__, new_p, t);
+                }
+
+
+                var moveGenTo = function( string_pt, string_pt2, _new_p, _t){
+                    if( _t > 0 && _t < 1){
+                        
+                        var t = _t || 1;
+                        pt[string_pt] = pt[string_pt].add(  _new_p.subtract(pt[string_pt]).multiply( new Point(t, t) ));
+                        part_arm.updateView();
+                    }
+                    return part_arm;
+                }
+
+
+                // Interface:
+                return init();
             }
+
+            // Front part of the arm, with the hand:
+            var arm_front = function(){
+                
+
+                var hand = function(pos){
+                    
+                    var handy = new Path(); handy.pathData = "M7.163,7.416c-3.267,3.988-9.146,4.573-13.135,1.307c-2.393-1.96-3.561-4.86-3.405-7.729c0.025-0.478,0.083-0.959,0.127-1.477c0.566-3.788-1.401-9.278,2.187-8.923c0.353-0.035,0.718-0.039,1.097-0.016c0.381,0.02,0.774,0.066,1.189,0.127c2.429-1.302,4.407-1.732,6.067-1.465c0.416,0.066,0.816,0.171,1.236,0.27c8.705-0.736,6.591,6.58,6.741,12.496c-0.028,0.481-0.092,0.96-0.191,1.433C8.777,4.854,8.144,6.221,7.163,7.416z";
+                    handy.set({
+                        strokeColor: '#000',
+                        strokeWidth: 1,
+                        fillColor: '#FFF0B3'
+                    });
+                    handy.closed = true; //handy.selected = true;
+                    handy.position = pos;
+
+                    return handy;
+                }
+
+
+                var draw = function(start, end){
+                    
+                    var armi = new Path([ start, end] );
+                    armi.style = {
+                        strokeColor: '#000',
+                        strokeWidth: 1
+                    };
+
+
+                    return [armi, hand(end) ];
+
+                }
+
+
+                return armGen('elbow', 'end', draw);
+            }
+
+
+            // back part of the arm
+            var arm_back = function(){
+                var tmp = armGen('begin', 'elbow');
+                
+
+                return tmp;
+            }
+
+            
+            // Interface:
+            return init();
+            
         }
-    }
 
 
-    var head = function(){
+        var head = function(){
 
             // Interface:
             return {
@@ -60,84 +347,94 @@ with (paper){
                 }
             }
         }
-     
-    var body = function(){
-     // Huge circle:
-        var big = new Path.Circle({
-            center: [200, 300],
-            radius: 100,
-        });
-        
-        // Small circle:
-        var small = new Path.Circle({
-            center: [200, 200],
-            radius: 70,
-        });
-        // Styling for all:
-        var style = {
-            fillColor: 'white',
-            strokeColor: 'black',
-            strokeWidth: 1
-        };
+         
+        var body = function(){
 
-        small.style = style;
-        big.style = style;
-        small.selected = true;
-        big.selected = true;
+            var _body = new Group();
+         // Huge circle:
+            var big = new Path.Circle({
+                center: [200, 300],
+                radius: 100,
+            });
+            
+            // Small circle:
+            var small = new Path.Circle({
+                center: [200, 200],
+                radius: 70,
+            });
+            // Styling for all:
+            var style = {
+                fillColor: 'white',
+                strokeColor: 'black',
+                strokeWidth: 1
+            };
 
-        var center1 = small.position;
-        var center2 = big.position;
-        var radius1 = small.bounds.width / 2;
-        var radius2 = big.bounds.width / 2;
-        var pi2 = Math.PI / 2;
-        var d = center1.getDistance(center2);
-        var u1 = Math.acos((radius1 * radius1 + d * d - radius2 * radius2) /
-                    (2 * radius1 * d)),
-            u2 = Math.acos((radius2 * radius2 + d * d - radius1 * radius1) /
-                    (2 * radius2 * d));
-        
+            small.style = style;
+            big.style = style;
+            small.selected = true;
+            big.selected = true;
 
-        
-        var angle1 = center2.getAngleInRadians(center1) + Math.PI/2.3;
-        var angle2 = Math.acos((radius1 - radius2) / d);
-        var angle1a = angle1 + u1 + (angle2 - u1) * 0.5;
-        var angle1b = angle1 - u1 - (angle2 - u1) * 0.5;
-        var angle2a = angle1 + Math.PI - u2 - (Math.PI - u2 - angle2) * 0.5;
-        var angle2b = angle1 - Math.PI + u2 + (Math.PI - u2 - angle2) * 0.5;
-        var p1a = center1.add(   getVector(angle1a, radius1));
-        var p1b = center1.add(   getVector(angle1b, radius1));
-        var p2a = center2.add(   getVector(angle2a, radius2));
-        var p2b = center2.add(   getVector(angle2b, radius2));
+            var center1 = small.position;
+            var center2 = big.position;
+            var radius1 = small.bounds.width / 2;
+            var radius2 = big.bounds.width / 2;
+            var pi2 = Math.PI / 2;
+            var d = center1.getDistance(center2);
+            var u1 = Math.acos((radius1 * radius1 + d * d - radius2 * radius2) /
+                        (2 * radius1 * d)),
+                u2 = Math.acos((radius2 * radius2 + d * d - radius1 * radius1) /
+                        (2 * radius2 * d));
+            
+
+            
+            var angle1 = center2.getAngleInRadians(center1) + Math.PI/2.3;
+            var angle2 = Math.acos((radius1 - radius2) / d);
+            var angle1a = angle1 + u1 + (angle2 - u1) * 0.5;
+            var angle1b = angle1 - u1 - (angle2 - u1) * 0.5;
+            var angle2a = angle1 + Math.PI - u2 - (Math.PI - u2 - angle2) * 0.5;
+            var angle2b = angle1 - Math.PI + u2 + (Math.PI - u2 - angle2) * 0.5;
+            var p1a = center1.add(   getVector(angle1a, radius1));
+            var p1b = center1.add(   getVector(angle1b, radius1));
+            var p2a = center2.add(   getVector(angle2a, radius2));
+            var p2b = center2.add(   getVector(angle2b, radius2));
 
 
 
-        // define handle length by the distance between
-        // both ends of the curve to draw
-        var totalRadius = (radius1 + radius2);
-        var d2 = Math.min(0.5 * 2.4, (p1a.subtract(p2a)).length / totalRadius);
+            // define handle length by the distance between
+            // both ends of the curve to draw
+            var totalRadius = (radius1 + radius2);
+            var d2 = Math.min(0.5 * 2.4, (p1a.subtract(p2a)).length / totalRadius);
 
-        // case circles are overlapping:
-        d2 *= Math.min(1, d * 2 / (radius1 + radius2));
+            // case circles are overlapping:
+            d2 *= Math.min(1, d * 2 / (radius1 + radius2));
 
-        radius1 *= d2;
-        radius2 *= d2;
+            radius1 *= d2;
+            radius2 *= d2;
 
-        var path = new Path({
-            segments: [p1a, p2a, p2b, p1b],
-            style:  {
-                fillColor: 'black',
-            },
-            closed: true
-        });
-        var segments = path.segments;
-        segments[0].handleOut = getVector(angle1a - pi2, radius1);
-        segments[1].handleIn = getVector(angle2a + pi2, radius2);
-        segments[2].handleOut = getVector(angle2b - pi2, radius2);
-        segments[3].handleIn = getVector(angle1b + pi2, radius1);
+            var path = new Path({
+                segments: [p1a, p2a, p2b, p1b],
+                style:  {
+                    fillColor: 'black',
+                },
+                closed: true
+            });
+            var segments = path.segments;
+            segments[0].handleOut = getVector(angle1a - pi2, radius1);
+            segments[1].handleIn = getVector(angle2a + pi2, radius2);
+            segments[2].handleOut = getVector(angle2b - pi2, radius2);
+            segments[3].handleIn = getVector(angle1b + pi2, radius1);
 
-        path.selected = true;
-    }
+            path.selected = true;
 
+            _body.addChildren([big, small, path]);
+            return _body;
+        }
+
+
+        // Interface:
+        return init();
+
+    } // End Dancer
 
     // ------------------------------------------------
     function getVector(radians, length) {
